460c3da4a5bdfdbb7ea1814cd0db412602bd16b9
Den Sumbaev <d.sumbaev@ispsystem.com>
2019-09-03 15:52:46 +0800

    docs(translang): add tsdoc info to functions

--- packages/translang/src/index.ts
+++ packages/translang/src/index.ts
@@ -1,6 +1,13 @@
 import Polyglot from 'node-polyglot';
 import { Path } from './types';
 
+/**
+ * Function to get a message in some lang
+ * 
+ * @param polyglot - Polyglot object
+ * @param keys - path to a message
+ * @param options - Polyglot options
+ */
 function msg(polyglot: Polyglot, keys: string[], options?: number | Polyglot.InterpolationOptions): string {
   if (Array.isArray(keys)) {
     return polyglot.t(keys.join('.'), options);
@@ -12,6 +19,12 @@ function msg(polyglot: Polyglot, keys: string[], options?: number | Polyglot.Int
   return '';
 }
 
+/**
+ * Create Translang object
+ * 
+ * @param lang - language key, e.g. 'en'
+ * @param phrases - object with messages
+ */
 export function translang<Lang, LanguageTypes = string>(lang: LanguageTypes, phrases: object): Translang<Lang, LanguageTypes> {
   const _polyglot = new Polyglot({ phrases, locale: String(lang) });
 
@@ -21,6 +34,9 @@ export function translang<Lang, LanguageTypes = string>(lang: LanguageTypes, phr
   };
 }
 
+/**
+ * Interface with function to get a message in some lang
+ */
 export interface Translang<Lang, LanguageTypes = string> {
   msg<T extends Lang, L extends Path<T, L>>(params: L, options?: number | Polyglot.InterpolationOptions): string;
   lang: LanguageTypes;
